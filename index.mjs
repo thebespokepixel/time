c8597d4c45a87ee6bed2053d0ba7f67557727835
Mark Griffiths <mark@thebespokepixel.com>
2019-06-13 18:07:45 +0100

    Update Dependencies

--- index.mjs
+++ index.mjs
@@ -1,6 +1,3 @@
-import dateformat from 'dateformat';
 export { default as bespokeTimeFormat } from 'dateformat';
-import moment from 'moment';
 export { default as bespokeTime } from 'moment';
-import { DateTime, Interval, Duration } from 'luxon';
-export { DateTime, Interval, Duration } from 'luxon';
+export { DateTime, Duration, Interval } from 'luxon';
